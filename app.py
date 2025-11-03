a256b3e7a882575aff8e75f67dd213fb52826d2f
Dadarzz <haidaralifawwaz@gmail.com>
2025-11-03 18:13:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,4 @@ def handle_message(data):
     send(data, broadcast=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, debug=True)
